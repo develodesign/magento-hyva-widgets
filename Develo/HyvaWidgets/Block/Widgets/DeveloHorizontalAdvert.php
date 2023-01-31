663a5fa489094d7d996a7e4d7a423b593c1de309
ZeinabAnshur <zeinab@develodesign.co.uk>
2023-01-31 16:31:07 +0000

    Added Develo HyvaWidgets Module

---
+++ Develo/HyvaWidgets/Block/Widgets/DeveloHorizontalAdvert.php
@@ -0,0 +1,9 @@
+<?php
+namespace Develo\HyvaWidgets\Block\Widgets;
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+class DeveloHorizontalAdvert extends Template implements BlockInterface
+{
+    protected $_template = "widget/develohorizontaladvert.phtml";
+}
+
---
+++ Develo/HyvaWidgets/Block/Widgets/DeveloQuotesBlock.php
@@ -0,0 +1,9 @@
+<?php
+namespace Develo\HyvaWidgets\Block\Widgets;
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+class DeveloQuotesBlock extends Template implements BlockInterface
+{
+    protected $_template = "widget/develoquotesblock.phtml";
+}
+
