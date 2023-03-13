af3c47b1e3c03a0e215e25e33319210b7fd38dba
ZeinabAnshur <102522996+ZeinabAnshur@users.noreply.github.com>
2023-03-13 16:48:58 +0000

    Delete Develodesign/CmsWidgets directory

--- Develodesign/CmsWidgets/Block/Widget/DeveloQuotesBlock.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace  Develodesign\CmsWidgets\Block\Widget;
-use Magento\Framework\View\Element\Template;
-use Magento\Widget\Block\BlockInterface;
-class DeveloQuotesBlock extends Template implements BlockInterface
-{
-    protected $_template = "widget/develoquotesblock.phtml";
-}
-
