e3c59b82541f749523ca719ca9ebc6ea1cad3718
ZeinabAnshur <102522996+ZeinabAnshur@users.noreply.github.com>
2023-06-05 10:33:51 +0100

    Delete Block/Widget directory

--- Block/Widget/QuotesBlock.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace  Develodesign\HyvaCmsWidgets\Block\Widget;
-use Magento\Framework\View\Element\Template;
-use Magento\Widget\Block\BlockInterface;
-class QuotesBlock extends Template implements BlockInterface
-{
-    protected $_template = "widget/quotesblock.phtml";
-}
-
